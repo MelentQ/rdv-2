cd95cb834ea7f6c78b68cdcfa7063594d7402908
MelentQ <mejleht@yandex.ru>
2022-11-26 15:11:54 +0300

    Правки

--- src/js/videoSlider.js
+++ src/js/videoSlider.js
@@ -1,28 +1,39 @@
-import { Swiper } from 'swiper';
+import {Swiper, Scrollbar} from 'swiper';
 
 export default function videoSlider() {
   const containers = document.querySelectorAll('.js-video-slider');
 
   containers.forEach(container => {
-    new Swiper(container, {
-      speed: 400,
-      slidesPerView: 1.5,
-      spaceBetween: 16,
-      cssMode: true,
-      on: {
-        init: function (swiper) {
-          swiper.el.classList.remove("loading")
+    const slides = container.querySelectorAll('.swiper-slide');
+    if (slides.length > 1) {
+      new Swiper(container, {
+        speed: 400,
+        slidesPerView: 1.5,
+        spaceBetween: 16,
+        modules: [Scrollbar],
+        scrollbar: {
+          el: '.swiper-scrollbar',
+          draggable: true,
         },
-      },
-      breakpoints: {
-        670: {
-          spaceBetween: 24
+        on: {
+          init: function (swiper) {
+            swiper.el.classList.remove("loading")
+          },
         },
-        1100: {
-          slidesPerView: 2,
-          spaceBetween: 30
+        breakpoints: {
+          670: {
+            spaceBetween: 24
+          },
+          1100: {
+            slidesPerView: 2,
+            spaceBetween: 30
+          }
         }
-      }
-    });
+      });
+    } else {
+      container.classList.remove('loading');
+      const scrollbar = container.querySelector('.swiper-scrollbar');
+      scrollbar.remove();
+    }
   });
 }
